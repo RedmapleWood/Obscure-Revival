ac136932a7ff6dc8d8465c98a54934972c7ac055
RedmapleWood <yinwenhan12345678@gmail.com>
2026-06-21 13:06:27 +0800

    Update ItemStackMixin.java

--- src/main/java/redmaple/obscurerevival/mixin/ItemStackMixin.java
+++ src/main/java/redmaple/obscurerevival/mixin/ItemStackMixin.java
@@ -1,9 +1,3 @@
-/*
-Obscure Revival © 2026 by Redmaple Wood is licensed under CC BY-NC-SA 4.0.
-To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
-(Optional but appreciated: If you include this mod in a modpack, consider letting me know!)
-*/
-
 package redmaple.obscurerevival.mixin;
 
 import net.minecraft.component.DataComponentTypes;
