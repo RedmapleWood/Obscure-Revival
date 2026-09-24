455806a0d1323f5898dfe6282588a31ca37f4720
RedmapleWood <yinwenhan12345678@gmail.com>
2026-09-24 17:25:56 +0800

    Delete src/main directory

--- src/main/java/redmaple/obscurerevival/refined_chain_armor/KeloidStateAccess.java
+++
@@ -1,9 +0,0 @@
-package redmaple.obscurerevival.refined_chain_armor;
-
-public interface KeloidStateAccess {
-    int obscure_revival$getKeloidTimer();
-    void obscure_revival$setKeloidTimer(int timer);
-
-    int obscure_revival$getKeloidFailCount();
-    void obscure_revival$setKeloidFailCount(int count);
-}
